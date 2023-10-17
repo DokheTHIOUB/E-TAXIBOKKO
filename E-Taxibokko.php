451a435dc88f61f83cc09b56879c743c510351e0
DokheTHIOUB <thioub1901@gmail.com>
2023-10-17 15:49:52 -0700

    formulaire_html

---
+++ E-Taxibokko.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="taxibokko.css">
+</head>
+<body>
+    <div class="div">
+        <div>
+    <form action=" " method="get">
+        <h2> Inscription </h2>
+        <h5> Votre chauffeur en un clic! </h5>
+        <input  class="bouton-continuer" type="submit" id="" value="Continuer avec Facebook " >
+        <p id="ou"> ou </p>
+        <label for=""> Email </label><br>
+        <input type="email" name="email" id="email" value="" required > <br> <br>
+        <label for=""> Mot de passe </label><br>
+        <input type="password" name=" motdepasse" id="" required>  <br><br>
+        
+        <input id="compte" type="submit" value=" J'ai déjà un compte">
+        <DIv> <button class="bouton-inscrire" type="submit"> S'inscrire </button> </DIv>
+    <div>  
+    </form> </div>
+    <form>
+    <h2> Bienvenue </h2>
+    <p> Finalisez votre inscription en renseignant les informations manquantes </p>
+    <label for=""> Prénom </label> <br>
+    <input type="text" name="" id=""><br>
+    <label for=""> Nom </label><br>
+    <input type="text" name="" id=""><br>
+    <label for=""> Téléphone </label><br>
+<input type="number"  min="0" max=""name="" id="" placeholder=" + 221 77 000 00 00 ">  <br>
+<p>  Ajouter un code promo </p>
+ <button class="bouton-inscrire" type="submit">S'inscrire</button> 
+</form> </div></div>
+</body>
+</html>
